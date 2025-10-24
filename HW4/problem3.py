7d24f446d67eaf591fe8e15d537f02b1a8fd1b6d
DanDaMan2121 <dannyrox04@gmail.com>
2025-10-24 18:13:48 -0400

    messy

--- HW4/problem3.py
+++ HW4/problem3.py
@@ -47,34 +47,7 @@ def avoidDupes(list, ans):
         return ans
     # print(list)
     pos1 = position(list[0])
-    offset1 = length - 1
-
-    for j in range(offset1):
-        currentVal = list[j + 1]
-        pos2 = position(currentVal)
-        distance1 = pos1.distance(pos2)
-        a = pow(distance1, 2)
-        offset2 = length - j - 2
-        for i in range(offset2):
-            print('run', i)
-            pos3 = position(list[j + i + 2])
-            distance2 = pos1.distance(pos3)
-            b = pow(distance2, 2)
-            distance3 = pos2.distance(pos3)
-            c = pow(distance3, 2)
-            # print(distance1, distance2, distance3)
-            print('distance1:', distance1, ' distance2:', distance2, ' distance 3:', distance3)
-            if a != 0 and b != 0 and c != 0:
-                if a == (b + c):
-                    ans += 1
-                    print('ding')
-                elif b == (a + c):
-                    ans += 1
-                    print('ding')
-                elif c == (a + b):
-                    ans += 1
-                    print('ding')
-                print(pos1.getPosition(), pos2.getPosition(), pos3.getPosition())
+    offset1 = length - 1  
     
                 
     
