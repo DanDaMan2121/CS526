036621c4bbf224169c78a609683da6237d2b4f4d
DanDaMan2121 <dannyrox04@gmail.com>
2025-11-23 22:39:44 -0500

    a

--- HW6/problem2.py
+++ HW6/problem2.py
@@ -4,26 +4,26 @@ import sys
 def currentExpSort(myList, exp):
     n = len(myList)
     output = [0] * (n)
-    count = [0] * (10)
+    occurrences = [0] * (10)
     # occurences of each digit at that exponent
     for i in range(0, n):
         index = myList[i] // exp
-        count[index % 10] += 1
-    # print(count)
+        occurrences[index % 10] += 1
+    
 
-    # cumulative sum of count
+    # cumulative sum of occurences 
     # this will give us the position to place the integer of that digit occurence in the output array
     for i in range(1, 10): 
-        count[i] += count[i - 1]
-    # print(count)
+        occurrences[i] += occurrences[i - 1]
+    
 
     i = n - 1
     while i >= 0:
         index = myList[i] // exp # This gives me the mod value at that exponent 9432 // 10 = 943
-        # count[index % 10] represents the number of occurences at the index 943 mod 10 = 3
+        # occurrences[index % 10] represents the number of occurrences at the index 943 mod 10 = 3
         # output[3 - 1]
-        output[count[index % 10] - 1] = myList[i] # placing all the integers that share the same digit occurence in togther and in the digit occurence order
-        count[index % 10] -= 1
+        output[occurrences[index % 10] - 1] = myList[i] # placing all the integers that share the same digit occurence togther and in the digit occurrence order
+        occurrences[index % 10] -= 1
         i -= 1
     # print(output)
     return output
